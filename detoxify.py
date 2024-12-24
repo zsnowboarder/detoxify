22b70d3013599dc699d0e3eadb8914fb44dd4988
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-23 16:39:29 -0800

    Add files via upload

---
+++ detoxify.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pip install detoxify
+
+
+# In[142]:
+
+
+# Import the Detoxify library
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from detoxify import Detoxify
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+#get data from ctvnews
+url = "https://www.ctvnews.ca/"
+response = requests.get(url)
+bs = BeautifulSoup(response.content, "html.parser")
+
+# Create an instance of the Detoxify model
+model = Detoxify('original')
+
+# init dict
+scores = []
+
+def analyze(text):
+    results = model.predict(text)
+    scores.append(results)
+    return results
+
+def map_sum_threat(value):
+    if value < 0.05:
+        return "None detected"
+    elif value >= 0.05 and value < 0.4:
+        return "Low"
+    elif value >= 0.4 and value < 0.6:
+        return "Medium"
+    else:
+        return "High"
+    
+
+
+# In[132]:
+
+
+# get the data from internet
+texts = bs.find_all('div', class_='c-list__item__text')
+web_text = []
+
+for text in texts:
+    sy = text.find('p', class_='c-list__item__description')
+    if sy:
+        web_text.append({'text':sy.get_text()})
+
+
+# # load the data. use this when no Internet data
+# data = { 'text': [ "You are a terrible person and I hate you.", "Have a great day!", "I can't believe you did that, you idiot." ] }
+# df = pd.DataFrame(data)
+# 
+
+# In[111]:
+
+
+st.title("Threat and Sentiment Detection")
+new_text = st.text_area("Enter your text for analysis", value="I am going to get a gun and shoot people at all schools.")
+df_new_text = pd.DataFrame({"text":[new_text]})
+
+df = pd.DataFrame(web_text)
+df = df.append(df_new_text, ignore_index=True)
+
+
+# In[113]:
+
+
+df["toxicity"] = df["text"].apply(analyze)
+
+
+# In[152]:
+
+
+# VaderSentiment Analysis
+array_dicts = []
+array_pred = []
+
+for row_index, row in df.iterrows():
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(row["text"])
+    array_dicts.append(vs)
+    if vs["compound"] < 0:
+        array_pred.append("Negative")
+    elif vs["compound"] > 0:
+        array_pred.append("Positive")
+    else:
+        array_pred.append("Neutral")
+df_vs = pd.DataFrame(array_dicts)
+df_vs["vs_score"] = array_pred
+
+df_scores["sum_threat"] = df_scores[["toxicity", "severe_toxicity", "threat"]].sum(axis=1)
+df_scores["Detected Threat"] = df_scores["sum_threat"].apply(map_sum_threat)
+
+df = pd.concat([df, df_scores, df_vs], axis=1)
+
+
+# In[154]:
+
+
+df = df[["text", "vs_score", "Detected Threat"]]
+st.table(df)
+
+
+# In[ ]:
+
+
+
+
