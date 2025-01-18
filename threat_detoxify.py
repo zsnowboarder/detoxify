810028b3cf78838865fbc361e5cb486f464db061
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2025-01-17 18:29:52 -0800

    Update threat_detoxify.py

--- threat_detoxify.py
+++ threat_detoxify.py
@@ -105,6 +105,7 @@ if st.button("Analyze"):
     df = df[["text", "vs_score", "Detected Threat", "sum_threat"]]
     df = df.sort_values(by="sum_threat", ascending=False)
     st.table(df)
+    st.cache_data.clear() #clear streamlit cache
 
 
 # In[30]:
